79e5962e185d1272a2c43e4ddafabef94b3014f9
Viktor Osadchuk <viktor.osadchuk@plumrocket.com>
2020-04-19 21:29:11 +0300

    Add Set/Get Data Methods

--- src/DataObject.php
+++ src/DataObject.php
@@ -5,5 +5,35 @@ namespace MjFoxCore;
 
 class DataObject
 {
+    protected array $data;
 
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+
+    public function getData($value)
+    {
+        return $this->data[$value];
+    }
+
+    public function setData($key, $value)
+    {
+        return $this->data[$key] = $value;
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (substr($name, 0,3) === 'set')  {
+            $key = substr($name,3);
+            $this->setData($key,$arguments[0]);
+
+        }
+        if (substr($name, 0,3) === 'get') {
+            $key = substr($name,3);
+            return $this->getData($key);
+        }
+        return $this;
+    }
 }
